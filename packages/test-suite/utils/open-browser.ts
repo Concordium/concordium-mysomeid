3eeb12812d97cf18262d9c1e2a4be7d15c5cfa43
krismoe42 <128125240+krismoe42@users.noreply.github.com>
2023-04-15 13:44:10 +0900

    Addressing PR feedback
    - Adding dockerised set up to run tests in simplified manner.
    - Added README.md to explain how to run test suite.
    - Replaced user-data folder with user-data.tar.gz to simply repository and support diffirent endian saved versions of the Chrome user data depending on the OS.

--- packages/test-suite/utils/open-browser.ts
+++ packages/test-suite/utils/open-browser.ts
@@ -2,7 +2,7 @@ import puppeteer, { Page, Browser } from 'puppeteer'; // import Puppeteer
 import * as path from 'path';
 
 // Path to the actual extension we want to be testing
-const extFolder = path.join(__dirname, '..', '..', 'chrome-ext', 'build');
+const extFolder = process.env.EXT_FOLDER ?? path.join(__dirname, '..', '..', 'chrome-ext', 'build');
 const ccdWalletExt = path.join('extensions', 'dist');
 
 // Tell puppeteer we want to load the web extension
@@ -11,15 +11,16 @@ const puppeteerArgs = [
   `--disable-extensions-except=${extFolder},${ccdWalletExt}`
 ];
 
-// console.log("args", puppeteerArgs);
+const userDataDir = path.join(process.cwd(), 'user-data');
+
+console.log("puppeteerArgs", puppeteerArgs);
+console.log('userDataDir', userDataDir);
 
 puppeteer.launch({
   headless: false,
   slowMo: 250,
   devtools: true,
-  userDataDir: path.join('user-data'),
+  userDataDir,
   args: puppeteerArgs,
 }).then();
-
-
 
