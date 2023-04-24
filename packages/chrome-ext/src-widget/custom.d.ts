82f29a028315248b7298b49892e75d82b62d0fd8
krismoe42 <128125240+krismoe42@users.noreply.github.com>
2023-04-24 22:32:13 +0900

    Revert "Mys-106 new logo"

--- packages/chrome-ext/src-widget/custom.d.ts
+++ packages/chrome-ext/src-widget/custom.d.ts
@@ -1,7 +1,6 @@
 declare module "*.svg" {
-  import React from "react";
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-  export default SVG;
+  const content: string;
+  export default content;
 }
 
 declare module '*.ttf';
