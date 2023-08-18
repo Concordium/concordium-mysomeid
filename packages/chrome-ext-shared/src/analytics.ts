7ff543064bf2ddd07651653174e11dbb489a011a
kristian <kristian@tesseractinvestment.com>
2023-08-18 14:43:43 +0900

    PR feedback to make Analytics into a class

--- packages/chrome-ext-shared/src/analytics.ts
+++ packages/chrome-ext-shared/src/analytics.ts
@@ -8,51 +8,60 @@ export type AnalyticsEvent <Type extends string, TOptions extends Record<string,
 }
 
 /**
- * Type-safe functional analytics utility which wraps the Matomo analytics system for usage in the 
+ * Type-safe class-based analytics utility which wraps the Matomo analytics system for usage in the 
  * Chrome extension and dApp.
  */
-export const createAnalytics = <T extends AnalyticsEvent<string, Record<string, any>>> (prefix: string, defaultArgs: Record<string, string> = {}) => {
-    let uniqueId: string | undefined;
-    return {
-        /**
-         * Set UniqueId can be used to set a id which can be used to correlate a user
-         * through different platforms.
-         * 
-         * Note that the ID mustn't correlate to user data so usually this is stored locally
-         * as a UUID on installation or user creation. 
-         */
-        setUniqueId: (uid: string) => {
-            uniqueId = uid;
-        },
-        track: (data: T): void => {
-            const {
-                type: actionType,
-                options,
-            } = data;
+export class Analytics<T extends AnalyticsEvent<string, Record<string, any>>> {
+    private uniqueId?: string;
 
-            const url = new URL("https://concordium.matomo.cloud/matomo.php");
-            const vars = Object.entries({...defaultArgs, ...(options ?? {})}).reduce( (acc, [k, v], index) => {
-                return {
-                    ...acc,
-                    [`${index + 1}`]: [k, v],
-                };
-            }, {});
-            url.searchParams.append('_cvar', JSON.stringify(vars));
-            url.searchParams.append('action_name', prefix + actionType);
-            url.searchParams.append('idsite', '6');
-            url.searchParams.append('rec', '1');
-            url.searchParams.append('rand', Math.round(Math.random()*99999999999).toString());
-            if ( uniqueId !== undefined && uniqueId.length > 0 ) {
-                url.searchParams.append('uid', uniqueId);
-            }
+    constructor(private prefix: string, private defaultArgs: Record<string, string> = {}) {
+    }
 
-            fetch(url.toString(),{
-                mode: 'no-cors',
-            }).then(response => {
-                logger.verbose("analytics response " + response.status);
-            }).catch(err => {
-                logger.error(err);
-            });
-        },
+    /**
+     * Set UniqueId can be used to set an id which can be used to correlate a user
+     * through different platforms.
+     * 
+     * Note that the ID mustn't correlate to user data so usually this is stored locally
+     * as a UUID on installation or user creation. 
+     */
+    setUniqueId = (uid: string) => {
+        this.uniqueId = uid;
+    };
+
+    track = (data: T): void => {
+        const {
+            type: actionType,
+            options,
+        } = data;
+
+        const url = new URL("https://concordium.matomo.cloud/matomo.php");
+        const vars = Object.entries({ ...this.defaultArgs, ...(options ?? {}) }).reduce((acc, [k, v], index) => {
+            return {
+                ...acc,
+                [`${index + 1}`]: [k, v],
+            };
+        }, {});
+        url.searchParams.append('_cvar', JSON.stringify(vars));
+        url.searchParams.append('action_name', this.prefix + actionType);
+        url.searchParams.append('idsite', '6');
+        url.searchParams.append('rec', '1');
+        url.searchParams.append('rand', Math.round(Math.random() * 99999999999).toString());
+        if (this.uniqueId !== undefined && this.uniqueId.length > 0) {
+            url.searchParams.append('uid', this.uniqueId);
+        }
+
+        fetch(url.toString(), {
+            mode: 'no-cors',
+        }).then(response => {
+            logger.verbose("analytics response " + response.status);
+        }).catch(err => {
+            logger.error(err);
+        });
     };
 }
+
+/**
+ * Method used to instanciate a Analytics object.
+ */
+export const createAnalytics = <T extends AnalyticsEvent<string, Record<string, any>>> (prefix: string, defaultArgs: Record<string, string> = {}) =>
+    new Analytics<T>(prefix, defaultArgs);
